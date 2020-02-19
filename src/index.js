f37cfd17f2ed612cb465fd939d31887691e3c55b
Alexey Shevchenko <kamikozz@mail.ru>
2020-02-19 22:50:47 +0300

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!arguments.length) return 0;
+  if (Array.isArray(array)) {
+    if (!array.length) return 0;
+    return Math.min.apply(null, array)
+  }
+  throw new Error('Invalid type of an array argument');
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!arguments.length) return 0;
+  if (Array.isArray(array)) {
+    if (!array.length) return 0;
+    return Math.max.apply(null, array);
+  }
+  throw new Error('Invalid type of an array argument');
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!arguments.length) return 0;
+  if (Array.isArray(array)) {
+    if (!array.length) return 0;
+    return array.reduce((prev, curVal) => prev + curVal, 0) / array.length;
+  }
+  throw new Error('Invalid type of an array argument');
 }
